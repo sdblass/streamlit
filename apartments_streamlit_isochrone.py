37a4d1d216d459e37752c20360b56351d1a04402
sdblass <93170092+sdblass@users.noreply.github.com>
2022-06-02 19:17:11 -0400

    Update apartments_streamlit_isochrone.py

--- apartments_streamlit_isochrone.py
+++ apartments_streamlit_isochrone.py
@@ -7,10 +7,11 @@
 import pandas as pd
 import numpy as np
 from chart_studio import plotly as py
+from haversine import haversine, Unit, inverse_haversine
 import plotly.graph_objects as go
 import plotly.express as px
 import urllib.parse
-import requests
+import requests, math, random
 import streamlit as st
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
@@ -20,9 +21,10 @@ from shapely.geometry.polygon import Polygon
 
 st.write('''
 # Let us help you find your next apartment!
-Did you get a new job in the DC/VA/MD area? Are you considering moving closer to work? Will paying more for an apartment closer to work to reduce your commute time actually make sense financially? Let us help you answer that question!
-In the fields below, please enter an address for your new job location and we will help you find your next apartment! Be sure to spell everything correctly! Caps are optional.
-In the final version, apartment listings will come from a real estate website like [apartments.com](https://www.apartments.com).
+Did you get a new job? Are you considering moving closer to work? Will paying more for an apartment closer to work to reduce your commute time actually make sense financially? Let us help you answer that question!
+In the fields below, enter an address for your new job location and we will locate apartments in that area categorized by commute time. Be sure to spell everything correctly! The form is case-insensitive.
+
+Note, this version is a proof-of-concept using randomly generated rental listings. The final version will use actual listings from a real estate website like [apartments.com](https://www.apartments.com).
 ''')
 
 
@@ -45,7 +47,7 @@ with st.form(key='user_info'):
     st.write(
     '''
     ## Enter your hourly wage ($ per hour)
-    This will let us determine how much your time spent commuting is worth to you. The more time spent commuting, the higher your effective rent will be.
+    This will let us determine how much your time spent commuting is worth to you. The more time you spend commuting, the higher your effective rent will be.
     ''')
 
     hourly_income = st.number_input('Hourly wage', value=40)
@@ -82,54 +84,27 @@ with st.form(key='user_info'):
 if submit_button:
 # PART 3 - Generate random apartment listings. This will be replaced with real listings in the actual deployment.    
     # st.write(apt_types)
-    def generate_listings(st=1000, one=1000, two=1000): # creates a df of apartment listings and geocoords for studio, 1-br, and 2-br, each with a range of rents
-        studio_low = 1500
-        one_br_low = 1700
-        two_br_low = 2200
+    def generate_map_data(workplace, distance, num_points=5000):
+        def rent_gen(kind):
+            studio_low = 1500
+            one_br_low = 1700
+            two_br_low = 2200
+            high = 1000
+            if kind == 'studio':
+             return np.random.randint(studio_low, studio_low + high + 1)
+            elif kind == '1_br': 
+             return np.random.randint(one_br_low, one_br_low + high + 1)
+            else:
+             return np.random.randint(two_br_low, two_br_low + high + 1)
 
-        # The high end will be low end + $1000
-        high = 1000
-        
-        # GPS limits
-        
-        NW = 39.10243088446053, -77.45517065148175
-        NE = 39.10243088446053, -76.9218509753054
-        SE = 38.79255073884452, -76.9218509753054
-        SW = 38.79255073884452, -77.45517065148175
-        
-        lat_range = NW[0] - SW[0]
-        long_range = NE[1] - NW[1]
-        
-        studios = []
-        one_br = []
-        two_br = []
-        
-        for i in range(st):
-            rent = np.random.randint(studio_low, studio_low + high + 1)
-            rand = np.random.random()
-            lat =  SW[0] + rand*lat_range
-            rand = np.random.random()
-            long = SW[1] + rand*long_range
-            studios += [[rent, lat, long, 'studio']]
-        for i in range(one):
-            rent = np.random.randint(one_br_low, one_br_low + high + 1)
-            rand = np.random.random()
-            lat =  SW[0] + rand*lat_range
-            rand = np.random.random()
-            long = SW[1] + rand*long_range
-            one_br += [[rent, lat, long, '1_br']]
-        for i in range(two):
-            rent = np.random.randint(two_br_low, two_br_low + high + 1)
-            rand = np.random.random()
-            lat =  SW[0] + rand*lat_range
-            rand = np.random.random()
-            long = SW[1] + rand*long_range
-            two_br += [[rent, lat, long, '2_br']]
-                        
-        results = pd.DataFrame(studios + one_br + two_br, columns = ['rent', 'lat', 'long', 'type'])
-        results.to_pickle('listings')
+        map_data = [inverse_haversine(workplace, distance*math.sqrt(random.random()), random.random()*2*math.pi, unit=Unit.MILES) for _ in range(num_points)]
+        map_data = pd.DataFrame(map_data, columns=['lat', 'long'])
+        map_data['type'] = [random.choice(['studio', '1_br', '2_br']) for _ in range(map_data.shape[0])]
 
-        return results
+
+        map_data['rent'] = map_data['type'].apply(lambda x: rent_gen(x))
+
+        return map_data
 
     def get_geocoords(address=address):
         workplace = urllib.parse.quote(address)
@@ -149,7 +124,7 @@ if submit_button:
     # https://github.com/plotly/plotly.py/issues/2485
     # https://github.com/plotly/plotly.js/issues/2813 ('Note that the array `marker.color` and `marker.size`', are only available for *circle* symbols.')
 
-    results = generate_listings()
+    results = generate_map_data(workplace, 25, 5000)
     results = results[(results.rent <= rental_range[1]) & (results.rent >= rental_range[0]) & (results.type.isin(apt_types))]
 
     if not len(results):
@@ -159,30 +134,15 @@ if submit_button:
         ''')
     else:
 
-        def isochrone_layer(mode, minutes, workplace=workplace): # minutes = '10,20,30' or '40'
+        def isochrone_layer(mode, minutes, workplace=workplace): # minutes = '10,20,30'
             profile = f'mapbox/{mode}'
             coordinates = str(workplace[1]) + ',' +  str(workplace[0])
             contours_minutes = f'contours_minutes={minutes}'
             r = requests.get(f'https://api.mapbox.com/isochrone/v1/{profile}/{coordinates}?{contours_minutes}&polygons=true&access_token={mapbox_access_token}').json()
-            results = []
-            if minutes == '10,20,30': colors = ['red', 'yellow', 'lime'] # colors go in most to least travel time
-            else: colors = ['purple']
             length = len(minutes.split(','))
-            for i in range(length):
-                results += [
-            {
-                        'source': {
-                            'type': "FeatureCollection",
-                            'features': [{
-                                'type': "Feature",
-                                'geometry': {
-                                    'type': "MultiPolygon",
-                                    'coordinates': [r['features'][i]['geometry']['coordinates']]                               
-                }
-                            }]
-                        },
-                                    'type': "fill", 'below': "traces", 'color': f"{colors[i]}", 'opacity': 0.15}]
-            return results
+
+            polygons = [Polygon(r['features'][i]['geometry']['coordinates'][0]) for i in range(length-1, -1, -1)] # create list in order of smallest to largest polygon
+            return polygons
 
         # Get the isochrone data
         ten_thirty_layer = isochrone_layer(mode, '10,20,30')
@@ -193,7 +153,17 @@ if submit_button:
         # https://github.com/plotly/plotly.js/issues/2813 ('Note that the array `marker.color` and `marker.size`', are only available for *circle* symbols.')
 
         # create list of polygons
-        polygons = [Polygon(ten_thirty_layer[i]['source']['features'][0]['geometry']['coordinates'][0][0]) for i in range(2, -1, -1)] + [Polygon(forty_layer[0]['source']['features'][0]['geometry']['coordinates'][0][0])]    
+        polys = isochrone_layer(mode, '10,20,30') + isochrone_layer(mode, '40')    
+
+        def create_plotly_isochrones(polygons, colors, opacity=0.15): # enter list of polygons and func will output code for plotly
+            assert len(polygons) == len(colors), 'check length of inputs'
+            layers = [(polygons[i] - polygons[i-1]).__geo_interface__ for i in range(len(polygons) - 1, 0, -1)]
+            layers += [polygons[0].__geo_interface__]
+
+            layers = [{'source': layers[i], 'type': 'fill', 'color': colors[i], 'opacity': opacity} for i in range(len(layers))]
+
+            return layers
+
                 
         def which_polygon(point, polygons): # returns bool if point in polygon. Will loop through polys from smallest to largest.
             point = Point(point) # create point
@@ -205,14 +175,20 @@ if submit_button:
             if not point_found: return 4
             else: return i    
         
-        results['polygon'] = results.apply(lambda x: which_polygon([x.long, x.lat], polygons), axis=1)
+        results['polygon'] = results.apply(lambda x: which_polygon([x.long, x.lat], polys), axis=1)
         results['adjusted_rent'] = (results['rent'] + (results['polygon'] +1) * 10 * 2 * 20 /60 * 40).astype('int') # 10 minute increments
 
 
         results['commute time (minutes)'] = results.apply(lambda x: '<10' if x.polygon == 0 else '10-20' if x.polygon == 1 else '20-30' if x.polygon == 2 else '30-40' if x.polygon == 3 else '>40', axis=1)
 
+# There is a bug in plotly where if you set the marker style, the color defaults to gray.
+# https://github.com/plotly/plotly.py/issues/2485
+# https://github.com/plotly/plotly.js/issues/2813 ('Note that the array `marker.color` and `marker.size`', are only available for *circle* symbols.')
+
+        results.rename(columns={'polygon':'zone'}, inplace=True)
+
         px.set_mapbox_access_token(mapbox_access_token)
-        fig = px.scatter_mapbox(results, lat="lat", lon="long", hover_name="type", hover_data=["rent"],
+        fig = px.scatter_mapbox(results, lat="lat", lon="long", hover_name="type", hover_data=["rent", "zone"],
                                 color="adjusted_rent", zoom=12, height=600)
 
         fig.update_layout(mapbox_style="light")
@@ -230,11 +206,13 @@ if submit_button:
                 showlegend = False
                 )  
             )
+
+
         fig.update_layout(
             mapbox = {
                 'style': "light",
                 'center': { 'lon': workplace[1], 'lat': workplace[0]},
-                'zoom': 12, 'layers': ten_thirty_layer + forty_layer
+                'zoom': 12, 'layers': create_plotly_isochrones(polys, ['blue', 'red', 'yellow', 'green'], 0.1) # enter colors from largest to smallest isochrone
             },
             margin = {'l':0, 'r':0, 'b':0, 't':0})
 
@@ -244,8 +222,8 @@ if submit_button:
         ## The cheapest apartments for each commute length
         ''')
         results = results.sort_values('adjusted_rent').groupby('commute time (minutes)').head(3)
-        results.index = results.groupby('polygon').cumcount() + 1
-        results.drop('polygon', axis=1, inplace=True)
+        results.index = results.groupby('zone').cumcount() + 1
+        results.drop('zone', axis=1, inplace=True)
         results = results.reset_index().rename(columns = {'index': 'rank', 'adjusted_rent': 'effective rent'})
 
         # Reorder columns to put rents next to one another
